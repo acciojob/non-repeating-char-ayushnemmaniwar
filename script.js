f2b779d66682faeb0ded6a2bbd5f4f2db776adb3
ayushnemmaniwar <68007760+ayushnemmaniwar@users.noreply.github.com>
2025-01-05 10:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	console.log(str);
 }
-const input = prompt("Enter a string");
+//const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
